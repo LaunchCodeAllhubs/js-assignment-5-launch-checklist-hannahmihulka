9b89f6cb0437c748d8eae0cdd021497199043ccd
Hannah Mihulka <hannahmihulka@gmail.com>
2023-07-31 18:30:06 -0500

    changed to correct color

--- scriptHelper.js
+++ scriptHelper.js
@@ -75,7 +75,7 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
   
     if (isReadyForLaunch) {
       launchStatus.textContent = 'Shuttle is Ready for Launch';
-      launchStatus.style.color = 'green';
+      launchStatus.style.color = 'rgb(65, 159, 106)';
     } else {
       launchStatus.textContent = 'Shuttle Not Ready for Launch';
       launchStatus.style.color = 'rgb(199,37,78)';
